6f7abdc0df8367cc8ed754d5dfc2be386f6c2e63
tangshuren <tang.shuren@kuyun.com>
2017-11-01 19:11:42 +0800

    增加swagger

---
+++ src/main/java/com/xiaohuli/hanlp/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.xiaohuli.hanlp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by admin on 2017/8/16.
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .title("新闻文本处理 API")
+                .description("新闻文本处理 API接口文档")
+                .termsOfServiceUrl("http://localhost:8080")
+                .contact("tangshuren")
+                .version("1.0.0")
+                .build();
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+}
--- src/main/java/com/xiaohuli/hanlp/controller/ApiController.java
+++ src/main/java/com/xiaohuli/hanlp/controller/ApiController.java
@@ -7,19 +7,35 @@ import com.hankcs.hanlp.seg.Viterbi.ViterbiSegment;
 import com.hankcs.hanlp.seg.common.Term;
 import com.xiaohuli.hanlp.HanlpApplication;
 import com.xiaohuli.hanlp.dto.RequestParam;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Controller
+@RestController
 public class ApiController {
 
-    @RequestMapping("/api/request")
-    @ResponseBody
+    @ApiOperation(value = "新闻文本提取接口", httpMethod = "POST" ,notes = "WillGet=1时， 只返回关键词列表（每个关键词及其出现的次数，KeyWordsLimit 为返回的关键词个数,这时 SentenceWordsLimit,SummaryWordsLimit 可以忽略)<br>" +
+            "WillGet=2时， 只返回分词结果，按原文的结构分词，段落，句子顺序，标点都不变(这时 SentenceWordsLimit,SummaryWordsLimit 可以忽略)<br>" +
+            "WillGet=3时， 返回按原文的结构分词的结果；同时返回重点句子；字数限制 SentenceWordsLimit 内，先按句子的重要性降序排列，然后按原文的顺序返回给我。 (这时SummaryWordsLimit 可以忽略)<br>" +
+            "WillGet=4时， 返回按原文的结构分词的结果、总字数限制在SentenceWordsLimit以内的重点句子、总字数限制在SummaryWordsLimit 以内的摘要。<br>")
+    @ApiImplicitParams(value = {
+            @ApiImplicitParam(name = "title", value = "标题", required = true, paramType = "query", dataType = "String"),
+            @ApiImplicitParam(name = "content", value = "正文", required = true, paramType = "query", dataType = "String"),
+            @ApiImplicitParam(name = "willGet", value = "返回结果类型", required = true, paramType = "query", dataType = "Integer"),
+            @ApiImplicitParam(name = "keyWordsLimit", value = "关键词个数", required = true, paramType = "query", dataType = "Integer"),
+            @ApiImplicitParam(name = "sentenceWordsLimit", value = "重点句子的字数限制", paramType = "query", dataType = "Integer"),
+            @ApiImplicitParam(name = "summaryWordsLimit", value = "摘要文本的字数限制",  paramType = "query", dataType = "Integer")
+    })
+    @PostMapping("/api/request")
     public Map<String,Object> request(RequestParam param){
 
         Map<String,Object> resultMap = new HashMap<>();
